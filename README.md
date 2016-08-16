e83aa0dd2f72843d082f9070f3ad70c6bd3d3b4d
Dmitrii Gerasimenko <kiddima@gmail.com>
2016-08-16 20:11:08 +0300

    Fully refactored with asyncio

---
+++ README.md
@@ -0,0 +1,21 @@
+# rrtop100
+
+RadioRecord Top Hits Downloader
+
+
+# Installation
+
+If you don't use `pipsi`, you're missing out.
+Here are [installation instructions](https://github.com/mitsuhiko/pipsi#readme).
+
+Simply run:
+
+    $ pipsi install .
+
+
+# Usage
+
+To use it:
+
+    $ rrtop100 --help
+
---
+++ setup.py
@@ -0,0 +1,48 @@
+"""
+RadioRecord Top Hits Downloader
+"""
+from setuptools import find_packages, setup
+
+dependencies = ['click==6.6', 'aiohttp==0.22.5']
+
+setup(
+    name='rrtop100',
+    version='0.1.0',
+    url='https://github.com/kidig/rrtop100',
+    license='BSD',
+    author='Dmitrii Gerasimenko',
+    author_email='kiddima@gmail.com',
+    description='RadioRecord Top Hits Downloader',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'rrtop100 = rrtop.cli:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
